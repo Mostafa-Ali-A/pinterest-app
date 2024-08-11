aeb4d4bb102faff6e825b4b57daae5f3aa3e98aa
mostafa-ali-a <mlovelylord@gmail.com>
2024-08-11 16:09:59 +0300

    Apply Semantic HTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
 import Provider from './Provider';
 
